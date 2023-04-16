224f9befdfaafdb9ffaf745e29fb9156afc5cbb0
Nasos Iliopoulos <2938701+nasosi@users.noreply.github.com>
2023-04-16 11:43:24 -0400

    Perform some cleanup when changing active app.

--- src/001_simple/handleActiveApp.cmake
+++ src/001_simple/handleActiveApp.cmake
@@ -2,6 +2,9 @@ get_filename_component( CurrentAppName "${CMAKE_CURRENT_LIST_DIR}" NAME )
 
 message( STATUS "Setting active app: " ${CurrentAppName} )
 
+file( REMOVE_RECURSE ${C4ProgramRootDir}/Data/Application)
+file( REMOVE_RECURSE ${C4ProgramRootDir}/Import/Application)
+
 file( MAKE_DIRECTORY  ${C4ProgramRootDir}/Data/Application )
 file( MAKE_DIRECTORY  ${C4ProgramRootDir}/Import/Application )
 
@@ -16,7 +19,7 @@ file( REMOVE_RECURSE ${C4ProgramRootDir}/Import/Application/${CurrentAppName} )
 C4CreateSymlink( ${C4ProgramRootDir}/Import/Application/${CurrentAppName} ${CMAKE_CURRENT_LIST_DIR}/Import )
 
 if (${C4EnableBuildTimeImport})
-    set( ImportDir                "Application/${CurrentAppName}/Game" )
+    set( ImportDir               "Application/${CurrentAppName}/Game" )
     set( ImportStringFileName    "gameStrings" )
     C4AddStringImportTarget( ${C4BinaryDestDir} ${ImportDir} ${ImportStringFileName} C4LatestImportTarget)
 
@@ -30,6 +33,7 @@ if ( C4PackUponInstall )
     # If data gets too big, the pak file may need to be broken up
     install( CODE "execute_process( COMMAND C4Engine pack Application\; quit WORKING_DIRECTORY ${C4BinaryDestDir})")
     install( FILES ${C4BinaryDestDir}/Data/Application.pak DESTINATION bin/Data)
+    install( CODE "file(REMOVE ${C4BinaryDestDir}/Data/Application.pak )" )
 else()
     # Here we need to manually specify each of the Data folders
     install( DIRECTORY    
--- src/007_simple_animated_shader/handleActiveApp.cmake
+++ src/007_simple_animated_shader/handleActiveApp.cmake
@@ -2,6 +2,9 @@ get_filename_component( CurrentAppName "${CMAKE_CURRENT_LIST_DIR}" NAME )
 
 message( STATUS "Setting active app: " ${CurrentAppName} )
 
+file( REMOVE_RECURSE ${C4ProgramRootDir}/Data/Application)
+file( REMOVE_RECURSE ${C4ProgramRootDir}/Import/Application)
+
 file( MAKE_DIRECTORY  ${C4ProgramRootDir}/Data/Application )
 file( MAKE_DIRECTORY  ${C4ProgramRootDir}/Import/Application )
 
@@ -30,6 +33,7 @@ if ( C4PackUponInstall )
     # If data gets too big, the pak file may need to be broken up
     install( CODE "execute_process( COMMAND C4Engine pack Application\; quit WORKING_DIRECTORY ${C4BinaryDestDir})")
     install( FILES ${C4BinaryDestDir}/Data/Application.pak DESTINATION bin/Data)
+    install( CODE "file(REMOVE ${C4BinaryDestDir}/Data/Application.pak )" )
 else()
     # Here we need to manually specify each of the Data folders
     install( DIRECTORY    
